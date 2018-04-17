0e6a00b667709b621b85c3075247f2a7b6259d1a
wuchenkai <ckthewu@gmail.com>
2018-04-17 17:15:22 +0800

    app.ts整理

--- app/app.ts
+++ app/app.ts
@@ -13,9 +13,9 @@ import { AppService } from './services/app';
 import { ServicesManager } from './services-manager';
 import Utils from './services/utils';
 import electron from 'electron';
-import Raven from 'raven-js';
-import RavenVue from 'raven-js/plugins/vue';
-import RavenConsole from 'raven-js/plugins/console';
+// import Raven from 'raven-js';
+// import RavenVue from 'raven-js/plugins/vue';
+// import RavenConsole from 'raven-js/plugins/console';
 import VTooltip from 'v-tooltip';
 
 const { ipcRenderer, remote } = electron;
@@ -23,56 +23,58 @@ const { ipcRenderer, remote } = electron;
 const slobsVersion = remote.process.env.SLOBS_VERSION;
 const isProduction = process.env.NODE_ENV === 'production';
 
-// This is the development DSN
-let sentryDsn = 'https://8f444a81edd446b69ce75421d5e91d4d@sentry.io/252950';
 
-if (isProduction) {
-  // This is the production DSN
-  sentryDsn = 'https://6971fa187bb64f58ab29ac514aa0eb3d@sentry.io/251674';
+// TODO:raven、错误追踪相关代码
+// // This is the development DSN
+// let sentryDsn = 'https://8f444a81edd446b69ce75421d5e91d4d@sentry.io/252950';
 
-  electron.crashReporter.start({
-    productName: 'streamlabs-obs',
-    companyName: 'streamlabs',
-    submitURL:
-      'https://streamlabs.sp.backtrace.io:6098/post?' +
-      'format=minidump&' +
-      'token=e3f92ff3be69381afe2718f94c56da4644567935cc52dec601cf82b3f52a06ce',
-    extra: {
-      version: slobsVersion,
-      processType: 'renderer'
-    }
-  });
-}
+// if (isProduction) {
+//   // This is the production DSN
+//   sentryDsn = 'https://6971fa187bb64f58ab29ac514aa0eb3d@sentry.io/251674';
 
-if (isProduction || process.env.SLOBS_REPORT_TO_SENTRY) {
-  Raven.config(sentryDsn, {
-    release: slobsVersion,
-    dataCallback: data => {
-      // Because our URLs are local files and not publicly
-      // accessible URLs, we simply truncate and send only
-      // the filename.  Unfortunately sentry's electron support
-      // isn't that great, so we do this hack.
-      // Some discussion here: https://github.com/getsentry/sentry/issues/2708
-      const normalize = (filename: string) => {
-        const splitArray = filename.split('/');
-        return splitArray[splitArray.length - 1];
-      };
+//   electron.crashReporter.start({
+//     productName: 'streamlabs-obs',
+//     companyName: 'streamlabs',
+//     submitURL:
+//       'https://streamlabs.sp.backtrace.io:6098/post?' +
+//       'format=minidump&' +
+//       'token=e3f92ff3be69381afe2718f94c56da4644567935cc52dec601cf82b3f52a06ce',
+//     extra: {
+//       version: slobsVersion,
+//       processType: 'renderer'
+//     }
+//   });
+// }
 
-      if (data.exception) {
-        data.exception.values[0].stacktrace.frames.forEach((frame: any) => {
-          frame.filename = normalize(frame.filename);
-        });
+// if (isProduction || process.env.SLOBS_REPORT_TO_SENTRY) {
+//   Raven.config(sentryDsn, {
+//     release: slobsVersion,
+//     dataCallback: data => {
+//       // Because our URLs are local files and not publicly
+//       // accessible URLs, we simply truncate and send only
+//       // the filename.  Unfortunately sentry's electron support
+//       // isn't that great, so we do this hack.
+//       // Some discussion here: https://github.com/getsentry/sentry/issues/2708
+//       const normalize = (filename: string) => {
+//         const splitArray = filename.split('/');
+//         return splitArray[splitArray.length - 1];
+//       };
 
-        data.culprit = data.exception.values[0].stacktrace.frames[0].filename;
-      }
+//       if (data.exception) {
+//         data.exception.values[0].stacktrace.frames.forEach((frame: any) => {
+//           frame.filename = normalize(frame.filename);
+//         });
 
-      return data;
-    }
-  })
-    .addPlugin(RavenVue, Vue)
-    .addPlugin(RavenConsole, console, { levels: ['error'] })
-    .install();
-}
+//         data.culprit = data.exception.values[0].stacktrace.frames[0].filename;
+//       }
+
+//       return data;
+//     }
+//   })
+//     .addPlugin(RavenVue, Vue)
+//     .addPlugin(RavenConsole, console, { levels: ['error'] })
+//     .install();
+// }
 
 require('./app.less');
 
@@ -95,7 +97,7 @@ document.addEventListener('DOMContentLoaded', () => {
   if (isFloat) {
     ipcRenderer.on('closeWindow', () => windowsService.closeFloatWindow());
     servicesManager.listenMessages();
-  }else if (isChild) {
+  } else if (isChild) {
     ipcRenderer.on('closeWindow', () => windowsService.closeChildWindow());
     servicesManager.listenMessages();
   } else {
