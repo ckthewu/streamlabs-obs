25db074cc2662d05c81fa85331ce8c90e2553d61
wuchenkai <ckthewu@gmail.com>
2018-04-18 19:25:21 +0800

    浮层test

--- app/components/windows/Barrage.vue
+++ app/components/windows/Barrage.vue
@@ -1,5 +1,8 @@
 <template>
-    <div class="barrage-container">
+    <div
+      class="barrage-container"
+    >
+      <button class="ding" :click="handeClick">ding</button>
       <div class="barrage">test item</div>
       <div class="barrage">test item</div>
       <div class="barrage">test item</div>
@@ -12,7 +15,6 @@
 
 <style lang="less" scoped>
 .barrage-container {
-  background: #F00;
   display: flex;
   flex-direction: column;
   width: 100px;
@@ -20,9 +22,22 @@
   left: 0;
   top:0;
   // animation: ani 5s infinite;
-  // -webkit-app-region: drag;
-  user-select: none;
-  pointer-events: none;
+  -webkit-app-region: drag;
+  // user-select: none;
+  // pointer-events: none;
+}
+
+.ding {
+  background: white;
+}
+
+.dragable {
+  -webkit-app-region: drag;
+  background: red;
+}
+
+.undragable {
+  -webkit-app-region: no-drag;
 }
 
 .barrage {
--- app/components/windows/Barrage.vue.ts
+++ app/components/windows/Barrage.vue.ts
@@ -1,11 +1,37 @@
 import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
+import { remote } from 'electron';
 // import { Inject } from '../../util/injector';
 // import { IScenesServiceApi } from '../../services/scenes';
 // import { ISourcesServiceApi, TSourceType, TPropertiesManager } from '../../services/sources';
 // import { WindowsService } from '../../services/windows';
 import windowMixin from '../mixins/window';
 
+let hoverTimeout: any;
+
 @Component({ mixins: [windowMixin] })
 export default class Barrage extends Vue {
+  focusBarrage: boolean = false;
+  handleMouseIn(event: any) {
+    if (hoverTimeout) return;
+    console.log('in');
+    console.log(event);
+    hoverTimeout = setTimeout(() => {
+      this.focus();
+      hoverTimeout = undefined;
+    }, 1000);
+  }
+  focus() {
+    this.focusBarrage = true;
+  }
+  handleMouseOut(event: any) {
+    console.log('out');
+    console.log(event);
+    if (hoverTimeout) clearTimeout(hoverTimeout);
+    hoverTimeout = undefined;
+    this.focusBarrage = false;
+  }
+  handeClick() {
+    this.focusBarrage = !this.focusBarrage;
+  }
 }
